50dc9e3f6fc2bdb7298d38006efc7ceb70d1bb25
x7c1 <jmtaro+x7c1@gmail.com>
2019-08-24 22:20:06 +0900

    enable to generate rust codes

---
+++ sources/grpc-rust/grpc-rust-server/build.rs
@@ -0,0 +1,13 @@
+extern crate protoc_rust_grpc;
+
+fn main() {
+    let gen_dir = "/mnt/sources/grpc-rust/grpc-rust-gen";
+
+    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+        out_dir: &(gen_dir.to_owned() + "/src"),
+        includes: &["/mnt/sources/proto"],
+        input: &["/mnt/sources/proto/aaa.proto"],
+        rust_protobuf: true,
+        ..Default::default()
+    }).expect("protoc-rust-grpc");
+}
--- sources/grpc-rust/grpc-rust-server/src/main.rs
+++ sources/grpc-rust/grpc-rust-server/src/main.rs
@@ -1,3 +1,11 @@
+use grpc_rust_gen::aaa::EchoStatus;
+
 fn main() {
-    println!("Hello, world!");
+    let status = {
+        let mut status = EchoStatus::new();
+        status.set_code(200);
+        status.set_message("hello!".to_string());
+        status
+    };
+    println!("{:#?}", status);
 }
