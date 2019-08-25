9752331e5f07545310e94803418703b1d6e7ffc8
x7c1 <jmtaro+x7c1@gmail.com>
2019-08-26 00:48:34 +0900

    add client sample

---
+++ sources/grpc-rs/grpc-rs-server/src/client.rs
@@ -0,0 +1,23 @@
+#[macro_use]
+extern crate log;
+
+use std::sync::Arc;
+
+use grpcio::{ChannelBuilder, EnvBuilder};
+use grpc_rs_gen::{GreeterClient, HelloRequest};
+
+
+fn main() {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+    info!("client started...!!");
+
+    let env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::new(env).connect("grpc_server:50051");
+    let client = GreeterClient::new(ch);
+
+    let mut req = HelloRequest::default();
+    req.set_name("world".to_owned());
+    let reply = client.say_hello(&req).expect("rpc");
+    info!("Greeter received!!!: {}", reply.get_message());
+}
