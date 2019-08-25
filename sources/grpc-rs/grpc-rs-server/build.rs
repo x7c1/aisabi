7efd7e5d595fc11773c62908766ce3bf849cd835
x7c1 <jmtaro+x7c1@gmail.com>
2019-08-25 17:51:16 +0900

    add grpc-rs samples

---
+++ sources/grpc-rs/grpc-rs-server/build.rs
@@ -0,0 +1,15 @@
+extern crate protoc_grpcio;
+
+fn main() {
+    let gen_dir = "/mnt/sources/grpc-rs/grpc-rs-gen";
+    let proto_root = "/mnt/sources/proto";
+    println!("cargo:rerun-if-changed={}", proto_root);
+
+    protoc_grpcio::compile_grpc_protos(
+        &["/mnt/sources/proto/aaa.proto"],
+        &[proto_root, "/usr/local/include"],
+//        &proto_root,
+        &(gen_dir.to_owned() + "/src"),
+        None
+    ).expect("Failed to compile gRPC definitions!");
+}
---
+++ sources/grpc-rs/grpc-rs-server/src/main.rs
@@ -0,0 +1,27 @@
+use grpc_rs_gen::EchoStatus;
+
+fn main() {
+    let status = get_status();
+    println!("{:#?}", status);
+}
+
+fn get_status() -> EchoStatus {
+    EchoStatus {
+        code: 200,
+        message: "hello!".to_string(),
+        ..Default::default()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_status() {
+        let status = get_status();
+        main();
+        assert_eq!(status.get_code(), 200);
+        assert_eq!(status.get_message(), "hello!");
+    }
+}
