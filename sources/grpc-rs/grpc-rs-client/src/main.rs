64500729a6af4c1f0f07ecbbf3533e9945ad7bd6
x7c1 <jmtaro+x7c1@gmail.com>
2019-08-27 23:29:10 +0900

    separate client from server

--- sources/grpc-rs/grpc-rs-client/src/main.rs
+++ sources/grpc-rs/grpc-rs-client/src/main.rs
@@ -10,14 +10,17 @@ use grpc_rs_gen::{GreeterClient, HelloRequest};
 fn main() {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
-    info!("client started...!!");
+    info!("client started...!");
 
     let env = Arc::new(EnvBuilder::new().build());
     let ch = ChannelBuilder::new(env).connect("grpc_server:50051");
     let client = GreeterClient::new(ch);
 
-    let mut req = HelloRequest::default();
-    req.set_name("world".to_owned());
+    let mut req = HelloRequest {
+        name: "world".to_string(),
+        ..Default::default()
+    };
+//    req.set_name("world".to_owned());
     let reply = client.say_hello(&req).expect("rpc");
-    info!("Greeter received!!!: {}", reply.get_message());
+    info!("Greeter received: {}", reply.get_message());
 }
