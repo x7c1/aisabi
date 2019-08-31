cf34ff93f276951cd082f04bb641705d710b20ef
x7c1 <jmtaro+x7c1@gmail.com>
2019-08-31 18:37:11 +0900

    use relative paths

--- sources/tower-grpc/tower-grpc-gen/build.rs
+++ sources/tower-grpc/tower-grpc-gen/build.rs
@@ -2,16 +2,19 @@ use prost_build;
 
 fn main() {
     let mut config = prost_build::Config::new();
-    config.out_dir("/mnt/sources/tower-grpc/tower-grpc-gen/src/aisabi");
+    config.out_dir("./src/aisabi");
 
     tower_grpc_build::Config::from_prost(config)
         .enable_server(true)
         .enable_client(true)
         .build(
-            &["/mnt/sources/proto/greeter.proto"],
-            &["/mnt/sources/proto"],
+            &[
+                "../../proto/greeter.proto",
+                "../../proto/health_check.proto",
+            ],
+            &["../../proto"],
         )
         .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 
-    println!("cargo:rerun-if-changed=/mnt/sources/proto/greeter.proto");
+    println!("cargo:rerun-if-changed=../../proto/greeter.proto");
 }
