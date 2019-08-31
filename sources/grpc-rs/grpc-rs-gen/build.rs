cd433a5ed30e94f2487702be1fc2d5e517929e3f
x7c1 <jmtaro+x7c1@gmail.com>
2019-08-31 19:36:04 +0900

    use relative paths

--- sources/grpc-rs/grpc-rs-gen/build.rs
+++ sources/grpc-rs/grpc-rs-gen/build.rs
@@ -2,12 +2,13 @@ extern crate protoc_grpcio;
 
 fn main() {
     let gen_dir = "/mnt/sources/grpc-rs/grpc-rs-gen";
-    let proto_root = "/mnt/sources/proto";
+    let proto_root = "../../proto";
     println!("cargo:rerun-if-changed={}", proto_root);
 
     protoc_grpcio::compile_grpc_protos(
         &[
-            "/mnt/sources/proto/greeter.proto",
+            "../../proto/greeter.proto",
+            "../../proto/health_check.proto",
         ],
         &[proto_root, "/usr/local/include"],
         &(gen_dir.to_owned() + "/src"),
--- sources/grpc-rust/grpc-rust-server/build.rs
+++ sources/grpc-rust/grpc-rust-server/build.rs
@@ -1,14 +1,17 @@
 extern crate protoc_rust_grpc;
 
 fn main() {
-    let gen_dir = "/mnt/sources/grpc-rust/grpc-rust-gen";
-
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
-        out_dir: &(gen_dir.to_owned() + "/src"),
-        includes: &["/mnt/sources/proto", "/usr/local/include"],
-        input: &["/mnt/sources/proto/aaa.proto"],
+        out_dir: &("./src"),
+        includes: &["../../proto", "/usr/local/include"],
+        input: &[
+            "../../proto/greeter.proto",
+            "../../proto/health_check.proto",
+        ],
         rust_protobuf: true,
         ..Default::default()
     })
     .expect("protoc-rust-grpc");
+
+    println!("cargo:rerun-if-changed={}", "../../proto");
 }
