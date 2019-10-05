9a46a4f3e1b4139c1347cc2dc26a3e3b11ee785a
x7c1 <jmtaro+x7c1@gmail.com>
2019-10-05 14:38:43 +0900

    avoid dependency on container environment

--- sources/grpc-rs/grpc-rs-gen/build.rs
+++ sources/grpc-rs/grpc-rs-gen/build.rs
@@ -1,7 +1,6 @@
 extern crate protoc_grpcio;
 
 fn main() {
-    let gen_dir = "/mnt/sources/grpc-rs/grpc-rs-gen";
     let proto_root = "../../proto";
     println!("cargo:rerun-if-changed={}", proto_root);
 
@@ -11,7 +10,7 @@ fn main() {
             "../../proto/health_check.proto",
         ],
         &[proto_root, "/usr/local/include"],
-        &(gen_dir.to_owned() + "/src"),
+        "./src",
         None,
     )
     .expect("Failed to compile gRPC definitions!");
