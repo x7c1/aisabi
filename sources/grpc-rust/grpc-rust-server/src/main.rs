55454bb2785628153721e2d0b0ecf213a46d4e25
x7c1 <jmtaro+x7c1@gmail.com>
2019-10-05 16:48:17 +0900

    add comments

--- sources/grpc-rust/grpc-rust-server/src/main.rs
+++ sources/grpc-rust/grpc-rust-server/src/main.rs
@@ -1,5 +1,28 @@
 use grpc_rust_gen::greeter::HelloRequest;
 
+/*
+use grpc_rust_gen::greeter::HelloReply;
+use grpc_rust_gen::greeter_grpc::Greeter;
+
+// cannot build
+
+struct GreeterImpl;
+
+impl Greeter for GreeterImpl {
+    fn say_hello(&self, _: ServerHandlerContext, req: ServerRequestSingle<HelloRequest>, resp: ServerResponseUnarySink<HelloReply>) -> grpc::Result<()> {
+        let mut r = HelloReply::new();
+        let name = if req.message.get_name().is_empty() {
+            "world"
+        } else {
+            req.message.get_name()
+        };
+        println!("greeting request from {}", name);
+        r.set_message(format!("Hello {}", name));
+        resp.finish(r)
+    }
+}
+*/
+
 fn main() {
     let status = get_request();
     println!("{:#?}", status);
